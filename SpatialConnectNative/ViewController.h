ccbef9dfcae2902275956bb84b08da92f3afcda7
Wes Richardet <wes@tetriscodes.com>
2015-10-31 13:30:09 -0500

    Initial Project Commit

---
+++ SpatialConnectNative/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpatialConnectNative
+//
+//  Created by Wes Richardet on 10/30/15.
+//  Copyright © 2015 Boundless Spatial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
